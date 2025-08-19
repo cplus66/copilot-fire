3b87cf2f73bc8bf2d8c760631b9197e2eee30b5a
Cplus Shen <cplus.shen@gmail.com>
2025-08-19 15:01:10 +0800

    supported up to date data retrieve and analysis.

--- tw-stock/twse/STOCK_DAY/download.sh
+++ tw-stock/twse/STOCK_DAY/download.sh
@@ -2,18 +2,30 @@
 DELAY=10
 
 SLIST="1233 1303 1473 1477 2103 2395 2912 6128 6184 8046"
-YEAR="2015 2016 2017 2018 2019 2020 2021 2022 2023 2024"
-MONTH="01 02 03 04 05 06 07 08 09 10 11 12"
-#YEAR="2025"
-#MONTH="01 02 03 04 05 06 07 08"
+SLIST="2330"
+
+start_year=2015
+start_month=1
+end_year=2025
+end_month=8
+
+year=$start_year
+month=$start_month
 
 for SID in $SLIST; do
-  for i in ${YEAR}; do
-    mkdir -p $SID/$i
-    for j in ${MONTH}; do
-      curl https://www.twse.com.tw/rwd/en/afterTrading/STOCK_DAY?date=${i}${j}01\&stockNo=${SID}\&response=csv \
-	      -o $SID/$i/STOCK_DAY_${SID}_${i}${j}.csv
-      sleep $DELAY
-    done
+  while [ $year -lt $end_year ] || { [ $year -eq $end_year ] && [ $month -le $end_month ]; }; do
+    y=$(printf %04d $year)
+    m=$(printf %02d $month)
+
+    mkdir -p $SID/$y
+    curl https://www.twse.com.tw/rwd/en/afterTrading/STOCK_DAY?date=${y}${m}01\&stockNo=${SID}\&response=csv \
+	      -o $SID/$y/STOCK_DAY_${SID}_${y}${m}.csv
+    sleep $DELAY
+
+    month=$((month + 1))
+    if [ $month -gt 12 ]; then
+      month=1
+      year=$((year + 1))
+    fi
   done
 done
